7764e4050c575db428d0ac37485b940136f3ed0d
LAIBA ASHRAF <112387588+LAIBAASHRAF@users.noreply.github.com>
2022-09-07 02:32:01 -0700

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,6 @@
+# use this link to print emoji(https://unicode.org/emoji/charts/emoji-list.html)
+# place 000 at + and place backslace at the start of unicode.
+print("\U0001F600")
+print("\U0001F601")
+print("\U0001F600", "\U0001F600")
+print("\U0001F600")
